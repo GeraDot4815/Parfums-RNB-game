0f09d52a90d3646a0a7632732821df712c91e7ce
Drakon-top <kyzy77777@yandex.ru>
2023-12-08 18:06:26 +0300

    alhpa memory

--- Assets/Memory/Scripts/Board.cs
+++ Assets/Memory/Scripts/Board.cs
@@ -1,12 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-public class Menu : MonoBehaviour
+
+public class Board : MonoBehaviour
 {
-    public void LoadPM(string sceneName) {
-        SceneManager.LoadScene(sceneName);
-    }
     // Start is called before the first frame update
     void Start()
     {
